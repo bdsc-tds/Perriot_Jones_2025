67b617188285d1fc8bbedd2b82d7c11e66b1d2fd
Helen Lindsay <helen.lindsay@chuv.ch>
2024-10-11 16:27:30 +0200

    testing integration methods for new data

---
+++ scripts/02_integrate_samples.R
@@ -0,0 +1,133 @@
+# Libraries and setup ----
+
+library("hdf5r")
+library("rhdf5")
+library("Seurat")
+library("SeuratDisk")
+library("tidyverse")
+
+set.seed(7620)
+
+min_cells <- 10
+min_features <- 100
+n_pcs <- 30
+n_integration_features <- 3000
+
+project_dir <- tryCatch({Sys.getenv()[["project_dir"]]},
+                        error = function(cond){return(".")})
+scratch_dir <- tryCatch({Sys.getenv()[["scratch_dir"]]},
+                        error = function(cond){return(".")})
+data_dir <- file.path(project_dir, "data")
+
+# Functions ----
+
+h5_to_seurat <- function(nm, exp_dir, min_cells, min_features){
+    
+    h5_data <- hdf5r::H5File$new(exp_dir, mode = 'r')
+    
+    decont_m <- Matrix::sparseMatrix(
+        i = h5_data[['matrix/indices']][],
+        p = h5_data[['matrix/indptr']][],
+        x = h5_data[['matrix/data']][],
+        dimnames = list(
+            h5_data[['matrix/features/name']][],
+            h5_data[['matrix/barcodes']][]
+        ),
+        dims = h5_data[['matrix/shape']][],
+        index1 = FALSE
+    )
+    
+    samples <- data.frame(Sample = rep(nm, ncol(decont_m)),
+                          row.names = colnames(decont_m))
+    
+    seurat_obj <- CreateSeuratObject(counts = decont_m[keep_rn, ], 
+                                     min.cells = min_cells,
+                                     min.features = min_features,
+                                     meta.data = samples)
+}    
+
+# Read data ----
+
+# Assume the combined_tcr file only contains the samples of interest
+combined_tcr <- read_rds(file.path(data_dir, "processed/combined_tcr.rds"))
+samples <- names(combined_tcr)
+
+# Read cellbender filtered data ----
+
+exp_dirs <- list.files(file.path(data_dir, "processed/cellbender/filtered"),
+                       full.names = TRUE,
+                       pattern = paste(samples, collapse = "|"))
+
+names(exp_dirs) <- gsub("_cellbender.*", "", basename(exp_dirs))
+
+# Get duplicated row names to drop
+# (note that after cellbender, samples do not have the same features)
+rn <- H5File$new(exp_dirs[[1]])[['matrix/features/name']][]
+rhdf5::h5closeAll()
+keep_rn <- ! ( duplicated(rn) | duplicated(rn, fromLast = TRUE) )
+
+# Create Seurat objects from cellbender output ----
+seurat_objs <- lapply(names(exp_dirs), function(nm){
+    obj <- h5_to_seurat(nm, exp_dirs[[nm]], min_cells, min_features)
+
+    obj[["percent.mt"]] <- Seurat::PercentageFeatureSet(obj,
+                                                        pattern = "^MT-")
+    obj
+})
+
+# Merge and plot umap prior to batch correction ----
+
+umap_ndim <- 20
+umap_unintegrated <- sprintf("umap_unintegrated_%s_dim.pdf", umap_ndim)
+
+if (! file.exists(umap_unintegrated)){
+    seurat_obj <- merge(seurat_objs[[1]], seurat_objs[2:length(seurat_objs)])
+    seurat_obj <- NormalizeData(seurat_obj)
+    seurat_obj <- FindVariableFeatures(seurat_obj,
+                                       nfeatures = n_integration_features)
+    all.genes <- rownames(seurat_obj)
+    seurat_obj <- ScaleData(seurat_obj)#, features = all.genes)
+    seurat_obj <- RunPCA(seurat_obj)
+    seurat_obj <- RunUMAP(seurat_obj, dims = 1:umap_ndim)
+
+    pdf(umap_unintegrated)
+    DimPlot(seurat_obj, reduction = "umap", group.by = "Sample")
+    dev.off()
+    
+    rm(seurat_obj)
+}
+
+# Run SCTransform ----
+
+sct_objs <- lapply(seurat_objs, function(obj){
+    SCTransform(obj,
+                vars.to.regress = "percent.mt",
+                return.only.var.genes = FALSE,
+                min_cells = 1,
+                method = "glmGamPoi") %>%
+        RunPCA(npcs = n_pcs)
+})
+
+names(sct_objs) <- names(exp_dirs)
+
+features <- SelectIntegrationFeatures(object.list = sct_objs,
+                                      nfeatures = n_integration_features)
+
+sct_objs <- PrepSCTIntegration(object.list = sct_objs,
+                               anchor.features = features)
+
+anchors <- FindIntegrationAnchors(object.list = sct_objs,
+                                  normalization.method = "SCT",
+                                  anchor.features = features)
+
+# Anchors file exists in data/processed
+write_rds(anchors, file = "anchors.rds")
+
+#all_features <- unique(unlist(lapply(sct_objects, Features)))
+
+integrated <- IntegrateData(anchorset = anchors,
+                            normalization.method = "SCT")#,
+                            #features.to.integrate = all_features)
+
+write_rds(integrated, file = "integrated.rds")
+
