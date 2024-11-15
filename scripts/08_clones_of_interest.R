0e6e77db8859251790b09946deedf177a1140417
Helen Lindsay <helen.lindsay@chuv.ch>
2024-11-15 16:24:59 +0100

    pca analyses for cluster 2

--- scripts/08_clones_of_interest.R
+++ scripts/08_clones_of_interest.R
@@ -2,8 +2,11 @@
 
 library("ComplexHeatmap")
 library("ggplot2")
+library("ggrepel")
+library("janitor")
 library("khroma") 
 library("patchwork")
+library("PCAtools")
 library("scales")
 library("Seurat")
 library("tidyverse")
@@ -130,41 +133,19 @@ temp <- cl2[[]] %>%
 cl2_top_clones <- subset(cl2, cells = Cells(cl2)[temp$top_clone])
 
 # Aggregate across sample and clone
-
+# Note that Seurat appears to use "average" by default -
+# AggregateExpression wraps PseudobulkExpression which has default method
+# "average"
 pseudo_cl2 <- AggregateExpression(cl2_top_clones,
                                   assays = "RNA",
                                   return.seurat = TRUE,
                                   group.by = c("Sample", "beta_aa"))
 
-pseudo_cl2_by_sample <- SplitObject(pseudo_cl2, split.by = "Sample")
-
-plots <- lapply(names(pseudo_cl2_by_sample), function(nm){
-    DotPlot(pseudo_cl2_by_sample[[nm]],
-            features = marker_set_2) +
-        theme(axis.text.y = element_text(size = 8),
-              axis.text.x = element_text(size = 4,
-                                         angle = 90,
-                                         hjust = 1,
-                                         vjust = 0.5)) +
-        coord_flip() + 
-        scale_color_viridis_c() +
-        labs(title = nm)
-    
-})
-
-
-
-
-pdf(file.path(fig_dir, "dotplots_top_clones.pdf"),
-    height = 15, width = 15)
-wrap_plots(plots) + plot_layout(guides = "collect")
-dev.off()
-
-
 anno <- pseudo_cl2[[]][, c("Sample", "beta_aa")]
 mtx <- GetAssayData(pseudo_cl2)[marker_set_2, ]
 
 row_anno = rowAnnotation(Sample = anno$Sample)
+
 
 # Raw expression
 Heatmap(t(mtx),
@@ -180,13 +161,129 @@ Heatmap(scale(t(mtx)),
 
 dev.off()
 
-# Are clones specific to cluster 2 or distributed across clusters ----
-
 
 # Differential expression between clone of interest and others ----
 
 cl2$is_coi = ifelse(cl2$coi == "no", "non-coi", "coi")
 
+Idents(cl2) <- cl2$Sample
+cl2_ri01_02 <- subset(cl2, idents = c("Ri01_dis", "Ri02"))
+Idents(cl2_ri01_02) <- cl2_ri01_02$is_coi
+
+coi_v_other_cl2_ri_samples <- FindAllMarkers(object = cl2_ri01_02) %>%
+    dplyr::filter(p_val_adj <= 0.05, cluster == "coi") %>%
+    dplyr::select(-cluster) %>%
+    dplyr::relocate(gene)
+
+readr::write_csv(coi_v_other_cl2_ri_samples,
+                 file = file.path(fig_dir, "de_coi_v_other_cl2_ri01_dis_ri02.csv"))
+
+# Heatmap of significant genes, grouped by clone ----
+
+# TCR by cell and sample
+
+# Top clones
+ri01_02_top_clones <- cl2_ri01_02[[]] %>%
+    dplyr::semi_join(cl2_top_clones[[]])
+
+# Check whether any clones are top in one sample but not in the other
+cl2_ri01_02[[]] %>%
+    dplyr::filter(beta_aa %in% ri01_02_top_clones$beta_aa) %>%
+    dplyr::anti_join(ri01_02_top_clones)
+    
+Idents(cl2_ri01_02) <- cl2_ri01_02$beta_aa
+cl2_ri01_02_top_clones <- subset(cl2_ri01_02,
+                                 cells = Cells(cl2_ri01_02)[cl2_ri01_02$beta_aa %in% 
+                                     ri01_02_top_clones$beta_aa],
+                                 features = coi_v_other_cl2_ri_samples$gene)
+
+
+Idents(cl2_ri01_02_top_clones) <- cl2_ri01_02_top_clones$beta_aa
+
+pdf(file.path(fig_dir, "heatmap_cl2_ri01_02_top_clones.pdf"),
+    width = 12)
+DoHeatmap(cl2_ri01_02_top_clones,
+               features = coi_v_other_cl2_ri_samples$gene,
+          angle = 90, size = 2) +
+    guides(color = FALSE) +
+    theme(axis.text.y = element_text(size = 6))
+dev.off()
+
+
+
+
+
+# Replicate heatmap
+scale_data <- GetAssayData(cl2_ri01_02_top_clones, layer = "scale.data")
+colnames(scale_data) <- NULL
+
+column_ha = HeatmapAnnotation(clone = cl2_ri01_02_top_clones$beta_aa)
+dend1 = cluster_between_groups(scale_data, cl2_ri01_02_top_clones$beta_aa)
+
+Heatmap(scale_data,
+        cluster_columns = dend1,
+        show_row_dend = FALSE,
+        show_column_dend = FALSE,
+        row_names_gp = gpar(fontsize = 8),
+        top_annotation = column_ha)
+
+# By clone of interest by cell and sample
+Idents(cl2_ri01_02) <- cl2_ri01_02$coi
+pdf(file.path(fig_dir, "heatmap_coi_v_other_cl2_ri01_dis_ri02.pdf"))
+DoHeatmap(cl2_ri01_02,
+          features = coi_v_other_cl2_ri_samples$gene) 
+dev.off()
+
+# Differential expression coi per sample ----
+
+# Ri01_dis
+
+Idents(cl2) <- "Sample"
+cl2_ri01_dis <- subset(cl2, idents = c("Ri01_dis"))
+Idents(cl2_ri01_dis) <- cl2_ri01_dis$is_coi
+
+coi_v_other_cl2_ri01_dis <- FindAllMarkers(object = cl2_ri01_dis) %>%
+    dplyr::filter(p_val_adj <= 0.05, cluster == "coi") %>%
+    dplyr::select(-cluster) %>%
+    dplyr::relocate(gene)
+
+readr::write_csv(coi_v_other_cl2_ri01_dis,
+                 file = file.path(fig_dir, "de_coi_v_other_cl2_ri01_dis.csv"))
+
+
+# Ri01_5m
+
+Idents(cl2) <- "Sample"
+cl2_ri01_5m <- subset(cl2, idents = c("Ri01_5m"))
+Idents(cl2_ri01_5m) <- cl2_ri01_5m$is_coi
+
+coi_v_other_cl2_ri01_5m <- FindAllMarkers(object = cl2_ri01_5m) %>%
+    dplyr::filter(p_val_adj <= 0.05, cluster == "coi") %>%
+    dplyr::select(-cluster) %>%
+    dplyr::relocate(gene)
+
+readr::write_csv(coi_v_other_cl2_ri01_dis,
+                 file = file.path(fig_dir, "de_coi_v_other_cl2_ri01_5m.csv"))
+
+# Clone of interest, _dis v _5m 
+
+Idents(cl2) <- "Sample"
+cl2_ri01 <- subset(cl2, idents = c("Ri01_5m", "Ri01_dis"))
+Idents(cl2_ri01) <- "coi"
+cl2_ri01_coi <- subset(cl2_ri01, idents = c("Ri01"))
+Idents(cl2_ri01_coi) <- "Sample"
+
+ri01_dis_v_5m <- FindAllMarkers(object = cl2_ri01_coi) %>%
+    dplyr::filter(p_val_adj <= 0.1, cluster == "Ri01_dis") %>%
+    dplyr::select(-cluster) %>%
+    dplyr::relocate(gene)
+
+readr::write_csv(ri01_dis_v_5m,
+                 file = file.path(fig_dir, "de_ri01_dis_v_5m.csv"))
+
+
+
+# Other DE analyses ----
 Idents(cl2) <- cl2$is_coi
 coi_de <- FindAllMarkers(object = cl2) %>%
     dplyr::filter(p_val_adj <= 0.01, cluster == "coi") %>%
@@ -197,12 +294,181 @@ subcluster_de <- FindAllMarkers(object = cl2) %>%
     dplyr::filter(p_val_adj <= 0.01) %>%
     readr::write_csv(file = file.path(fig_dir, "de_cl2_subclusters.csv"))
 
-# # Pseudobulk - not enough samples? Plus error from DESeq2 for non-integer
-# coi_f <- gsub("-", "_", c(ri01_coi, ri02_coi))
-# pseudo_cl2$is_coi <- gsub("-", "_", pseudo_cl2$beta_aa) %in% coi_f
-# 
-# Idents(pseudo_cl2) <- "is_coi"
-# coi_de <- FindMarkers(object = pseudo_cl2, 
-#                       ident.1 = TRUE, 
-#                       ident.2 = FALSE,
-#                       test.use = "DESeq2")
+# Ri01_dis coi versus Ri01_dis other clones ----
+
+# Are the clones in cluster 2 unique to cluster 2?
+
+meta <- read_csv(file.path(project_dir,
+                           "results/default_res/integrated_sketch_rpca_md.csv"))
+
+meta_top_clones <- meta %>%
+    # Cluster proportions
+    dplyr::group_by(seurat_clusters) %>%
+    dplyr::mutate(n_cluster = n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(cluster_ppn = n_cluster/n()) %>%
+    
+    # Keep clones of interest
+    dplyr::semi_join(cl2_top_clones[[]] %>%
+                          dplyr::select(Sample, beta_aa)) %>%
+    
+    # Cluster distribution for clones of interest
+    dplyr::group_by(Sample, beta_aa) %>%
+    dplyr::mutate(n_sample_clone = n()) %>%
+    dplyr::group_by(Sample, beta_aa, seurat_clusters,
+                    n_cluster, n_sample_clone, cluster_ppn) %>%
+    dplyr::summarise(n_sample_clone_cluster = n()) %>%
+    dplyr::mutate(pct_sample_clone_cluster = 
+                      n_sample_clone_cluster/n_sample_clone * 100,
+                  
+                  # Adjust for different cluster sizes 
+                  adj_ppn = pct_sample_clone_cluster / cluster_ppn) %>%
+    dplyr::ungroup()
+
+
+for (sample in unique(meta_top_clones$Sample)){ 
+
+    pdf(file.path(fig_dir, "clones_by_cluster", 
+                  sprintf("top_clones_by_cluster_%s.pdf", sample)),
+        height = 10, width = 10)
+    
+    p <- ggplot(meta_top_clones %>% filter(Sample == sample),
+           aes(x = pct_sample_clone_cluster,
+               y = beta_aa,
+               fill = as.factor(seurat_clusters))) + 
+        geom_bar(stat = "identity", color = "black") + 
+        theme_bw() +
+        facet_wrap(~Sample, scales = "free_y")
+    
+    print(p)
+    
+    dev.off()
+}
+
+
+
+for (sample in unique(meta_top_clones$Sample)){ 
+    
+    pdf(file.path(fig_dir, "clones_by_cluster_adj", 
+                  sprintf("top_clones_by_cluster_adj_%s.pdf", sample)),
+        height = 10, width = 10)
+    
+    p <- ggplot(meta_top_clones %>% filter(Sample == sample),
+                aes(x = adj_ppn,
+                    y = beta_aa,
+                    fill = as.factor(seurat_clusters))) + 
+        geom_bar(stat = "identity", 
+                 position = "fill",
+                 color = "black") + 
+        theme_bw() +
+        facet_wrap(~Sample, scales = "free_y") +
+        labs(y = NULL, y = "Proportion, adjusted for cluster size")
+    
+    print(p)
+    
+    dev.off()
+}
+
+meta_top_clones %>%
+    janitor::clean_names() %>%
+    dplyr::select(- n_cluster) %>%
+    dplyr:: mutate(across(where(is.numeric),
+                          \(x) tibble::num(x, digits = 2))) %>%
+    write_csv(file.path(fig_dir, "top_clone_cluster_proportions.csv"))
+
+
+# PCA with markers of interest ----
+
+pca_markers <- c("KIR3DL1", "ANXA1", "IL7R", "CD74", "TOX")
+cl2_pca1 <- subset(cl2_top_clones, features = pca_markers)
+
+cl2_pca1 <- RunPCA(cl2_pca1, features = pca_markers)
+
+pdf(file.path(fig_dir, 
+              "subcluster_coi/pca_cl2_top_clones_markers_by_cell.pdf"), height = 10)
+FeaturePlot(cl2_pca1, reduction = "pca", features = pca_markers)
+dev.off()
+
+
+cl2_pca3 <- RunPCA(cl2_pca1, features = setdiff(pca_markers, "TOX"))
+
+pdf(file.path(fig_dir, 
+              "subcluster_coi/pca_cl2_top_clones_markers_no_tox_by_cell.pdf"))
+FeaturePlot(cl2_pca3, reduction = "pca", features = setdiff(pca_markers, "TOX"))
+dev.off()
+
+# Group similar clones
+cl2_pca3[[]] <- cl2_pca3[[]] %>%
+    dplyr::mutate(tg = gsub("(TRBV[0-9]+-)[0-9]", "\\1\\*", TCR2),
+                  tg = gsub("(.*TRBJ[0-9]+-)[0-9]", "\\1\\*", tg)) %>%
+    tidyr::separate_wider_delim(tg, delim = ".", names_sep = "_" ) %>%
+    dplyr::mutate(gp_nm = sprintf("%s.*.%s.%s", tg_1, tg_3, tg_4)) %>%
+    dplyr::select(-matches("^tg_[0-9]")) %>%
+    dplyr::arrange(gp_nm)
+
+pdf(file.path(fig_dir, 
+              "subcluster_coi/pca_cl2_top_clones_by_clone_no_tox_by_cell.pdf"),
+    width = 8, height = 10)
+
+# Colour by cluster name
+Idents(cl2_pca3) <- cl2_pca3$gp_nm
+DimPlot(cl2_pca3, reduction = "pca", pt.size = 1) +
+    theme(legend.position = "bottom",
+          legend.text = element_text(size = 8)) + 
+    guides(color = guide_legend(ncol = 3,
+                                override.aes = list(size = 3)))
+dev.off()
+
+
+# PCA with expression aggregated by clone ----
+
+Idents(cl2_pca1) <- cl2_pca1$beta_aa
+cl2_agg <- AggregateExpression(cl2_pca1, return.seurat = TRUE)
+cl2_agg_no_tox <- RunPCA(cl2_agg, features = setdiff(pca_markers, "TOX"))
+
+pdf(file.path(fig_dir, 
+              "subcluster_coi/pca_cl2_top_clones_markers_no_tox.pdf"))
+FeaturePlot(cl2_agg_no_tox,
+            reduction = "pca",
+            features = setdiff(pca_markers, "TOX"))
+dev.off()
+
+cl2_agg_tox <- RunPCA(cl2_agg, features = pca_markers)
+
+pdf(file.path(fig_dir, 
+              "subcluster_coi/pca_cl2_top_clones_markers.pdf"), height = 10)
+FeaturePlot(cl2_agg_tox, reduction = "pca", features = pca_markers)
+dev.off()
+
+pca_coords <- Embeddings(cl2_agg_tox) %>%
+    tibble::as_tibble(rownames = "clone") %>%
+    dplyr::mutate(coi = clone %in% gsub("_", "-", c(ri01_coi, ri02_coi)))
+
+
+highlight_points = pca_coords %>% 
+    filter((PC_1 >= -0.25 & PC_1 <= 0.5 & PC_2 >= -0.75 & PC_2 <= 0.0) |
+              (PC_1 >= 0 & PC_1 <= 0.5 &  PC_2 >= 1.50 & PC_2 <= 2))
+    
+
+pdf(file.path(fig_dir, "subcluster_coi/pca_cl2_top_clones_with_tox.pdf"),
+    width = 10, height = 10)
+ggplot(pca_coords,
+       aes(x = PC_1, y = PC_2, color = coi)) +
+    geom_point(size = 2) +
+    theme_minimal() +
+    theme(axis.line = element_line(size = 0.5,
+                                     linetype = "solid",
+                                     colour = "black")) +
+    labs(x = "PC 1", y = "PC 2") +
+    scale_color_manual(values = c("lightgray", "red")) +
+    
+    geom_text_repel(data = highlight_points,
+                    aes(label = clone), size = 2.5,
+                    max.overlaps = Inf,
+                    min.segment.length = 0,
+                    color = "black",
+                    nudge_x = .15,
+                    nudge_y = .25) +
+    # Get rid of an outlier for better viewing
+    coord_cartesian(ylim = c(-2.5, 2.5))
+dev.off()
